fix gradients

derivatives.py
@@ -10,15 +10,16 @@ def quantization_derivative_min_level(x: np.ndarray, a: float, b: float, n_bits:
     # TODO: vectorize
 
     dQ = []
+    s = (b - a) / (2 ** n_bits - 1)
     for i in range(len(x)):
         x_i = x[i]
-        if x_i <= 0:
+        clip_elem = (x_i - a) / s
+        if clip_elem <= 0:
             d = 1
-        elif x_i >= 2 ** n_bits - 1:
-            d = 2 - (1 / 2 ** (n_bits - 1))
+        elif clip_elem >= 2 ** n_bits - 1:
+            d = 0
         else:
-            s = (b - a) / (2 ** (n_bits - 1))
-            d = 1 - (1 / (2 ** n_bits - 1) * np.round((x_i - a) / s)) + (x_i - b) / (b - a)
+            d = 1 - (np.round(clip_elem) / (2 ** n_bits - 1)) - ((b - x_i) / (b - a))
         dQ.append(d)
     return dQ
 
@@ -28,15 +29,16 @@ def quantization_derivative_max_level(x: np.ndarray, a: float, b: float, n_bits:
     # TODO: vectorize
 
     dQ = []
+    s = (b - a) / (2 ** n_bits - 1)
     for i in range(len(x)):
         x_i = x[i]
-        if x_i <= 0:
+        clip_elem = (x_i - a) / s
+        if clip_elem <= 0:
             d = 0
-        elif x_i >= 2 ** n_bits - 1:
-            d = 2 - (1 / 2 ** (n_bits - 1))
+        elif clip_elem >= 2 ** n_bits - 1:
+            d = 1
         else:
-            s = (b - a) / (2 ** (n_bits - 1))
-            d = (1 / (2 ** n_bits - 1) * np.round((x_i - a) / s)) + (a - x_i) / (b - a)
+            d = (np.round(clip_elem) / (2 ** n_bits - 1)) + ((a - x_i) / (b - a))
         dQ.append(d)
     return dQ
 
@@ -47,15 +49,16 @@ def quantization_derivative_threshold(x: np.ndarray, t: float, n_bits: int):
     # TODO: vectorize
 
     dQ = []
+    s = (2 * t) / (2 ** n_bits)
     for i in range(len(x)):
         x_i = x[i]
-        if x_i <= -t:
+        clip_elem = (x_i + t) / s
+        if clip_elem <= 0:
             d = -1
-        elif x_i >= 2 * t * (1 - 1 / (2 ** n_bits)):
-            d = 2 - (1 / 2 ** (n_bits - 1))
+        elif clip_elem >= 2 ** n_bits - 1:
+            d = 0
         else:
-            s = t / (2 ** (n_bits - 1))
-            d = (1 / (2 ** n_bits - 1)) * np.round((x_i / s)) + - x_i
+            d = -1 + (np.round(clip_elem) / (2 ** (n_bits - 1))) - x_i / t
         dQ.append(d)
     return dQ
 
@@ -70,6 +73,7 @@ def min_max_derivative(float_tensor: np.ndarray, a: float, b: float, n_bits: int
                     dQ=quantization_derivative_max_level(float_tensor, a=a, b=b, n_bits=n_bits))
 
     return np.asarray([dQ_da, dQ_db])
+
 
 def mse_derivative(x: np.ndarray, q: np.ndarray, dQ: np.ndarray):
     n = len(x)

experiments_framework.py
@@ -2,7 +2,7 @@ import numpy as np
 from typing import Callable, Dict, List
 
 from derivatives import mse_derivative, min_max_derivative
-from gradient_descent import gradient_descent, sgd
+from gradient_descent import gradient_descent, minibatch_sgd
 from model_compression_toolkit.common.quantization.quantizers.quantizers_helpers import \
     reshape_tensor_for_per_channel_search, uniform_quantize_tensor
 from model_compression_toolkit.common.similarity_analyzer import compute_mse
@@ -69,25 +69,25 @@ if __name__ == "__main__":
 
     init_param_fn = lambda x: np.asarray([np.min(x), np.max(x)])
 
-    optimizer = lambda x0, x: sgd(param=x0.copy(),
-                                  x=x.copy(),
-                                  loss_fn=loss_fn,
-                                  gradient=grad_fn,
-                                  n_epochs=30,
-                                  batch_size=10,
-                                  learn_rate=0.01,
-                                  tolerance=1e-06,
-                                  draw=False,
-                                  seed=2)
+    # optimizer = lambda x0, x: sgd(param=x0.copy(),
+    #                               x=x.copy(),
+    #                               loss_fn=loss_fn,
+    #                               gradient=grad_fn,
+    #                               n_epochs=30,
+    #                               batch_size=10,
+    #                               learn_rate=0.01,
+    #                               tolerance=1e-06,
+    #                               draw=False,
+    #                               seed=2)
 
-    # optimizer = lambda x0, x: gradient_descent(param=x0.copy(),
-    #                                            x=x.copy(),
-    #                                            loss_fn=loss_fn,
-    #                                            gradient=grad_fn,
-    #                                            n_iter=50,
-    #                                            learn_rate=0.01,
-    #                                            tolerance=1e-06,
-    #                                            draw=False)
+    optimizer = lambda x0, x: gradient_descent(param=x0.copy(),
+                                               x=x.copy(),
+                                               loss_fn=loss_fn,
+                                               gradient=grad_fn,
+                                               n_iter=50,
+                                               learn_rate=0.01,
+                                               tolerance=1e-06,
+                                               draw=False)
 
     loaded_weights = load_network_weights(model_name='mobilenetv2',
                                           layers=['block_2_depthwise_BN', 'block_8_project_BN', 'block_14_depthwise'])

gradient_descent.py
@@ -18,15 +18,15 @@ def draw_gd(n_iter, loss_res, param_res):
     plt.show()
     plt.cla()
 
-    plt.plot(range(n_iter), param_res)
-    plt.xlabel("Iteration")
-    # plt.xticks(range(n_iter), fontsize=6)
-    plt.ylabel("Parameter Value")
-    plt.show()
-    plt.cla()
+    # plt.plot(range(n_iter), param_res)
+    # plt.xlabel("Iteration")
+    # # plt.xticks(range(n_iter), fontsize=6)
+    # plt.ylabel("Parameter Value")
+    # plt.show()
+    # plt.cla()
 
 
-def sgd(param: np.ndarray, x: np.ndarray, loss_fn: Callable, gradient: Callable, n_epochs: int = 10,
+def minibatch_sgd(param: np.ndarray, x: np.ndarray, loss_fn: Callable, gradient: Callable, n_epochs: int = 10,
         learn_rate: float = 0.1, tolerance: float = 1e-06, batch_size: int = 1, seed=1, draw: bool = False):
     if batch_size > len(x):
         print("Batch size is larger then the number of data samples, folding to batch_size=1...")
@@ -125,23 +125,23 @@ def threshold_gd_example(weights_tensor, n_bits):
                            loss_fn=loss_fn,
                            gradient=grad_fn,
                            n_iter=30,
-                           learn_rate=0.08,
+                           learn_rate=0.005,
                            tolerance=1e-06,
                            draw=True)
 
-    batch_res = sgd(param=init_param.copy(),
-                    x=weights_tensor.copy(),
-                    loss_fn=loss_fn,
-                    gradient=grad_fn,
-                    n_epochs=50,
-                    learn_rate=0.001,
-                    tolerance=1e-06,
-                    batch_size=10,
-                    seed=2,
-                    draw=True)
+    # batch_res = minibatch_sgd(param=init_param.copy(),
+    #                           x=weights_tensor.copy(),
+    #                           loss_fn=loss_fn,
+    #                           gradient=grad_fn,
+    #                           n_epochs=50,
+    #                           learn_rate=0.001,
+    #                           tolerance=1e-06,
+    #                           batch_size=10,
+    #                           seed=2,
+    #                           draw=True)
 
     print(init_param)
-    print(res, batch_res)
+    print(res)
 
 
 def min_max_gd_example(weights_tensor, n_bits):
@@ -161,28 +161,29 @@ def min_max_gd_example(weights_tensor, n_bits):
                            tolerance=1e-06,
                            draw=True)
 
-    batch_res = sgd(param=init_param.copy(),
-                    x=weights_tensor.copy(),
-                    loss_fn=loss_fn,
-                    gradient=grad_fn,
-                    n_epochs=50,
-                    learn_rate=0.001,
-                    tolerance=1e-06,
-                    batch_size=10,
-                    seed=2,
-                    draw=True)
+    # batch_res = minibatch_sgd(param=init_param.copy(),
+    #                           x=weights_tensor.copy(),
+    #                           loss_fn=loss_fn,
+    #                           gradient=grad_fn,
+    #                           n_epochs=50,
+    #                           learn_rate=0.001,
+    #                           tolerance=1e-06,
+    #                           batch_size=10,
+    #                           seed=2,
+    #                           draw=True)
 
     print("Init: ", init_param)
-    print(res, batch_res)
+    print(res)
 
 
 if __name__ == "__main__":
     loaded_weights = load_network_weights(model_name='mobilenetv2',
-                                          layers=['block_2_depthwise_BN'])
-    weights_tensor = loaded_weights['block_2_depthwise_BN']
+                                          layers=['block_2_depthwise'])
+    weights_tensor = loaded_weights['block_2_depthwise']['weights']
     print(weights_tensor.shape)
+    weights_tensor = weights_tensor.flatten()
 
-    threshold_gd_example(weights_tensor, 8)
-    # min_max_gd_example(weights_tensor, 8)
+    # threshold_gd_example(weights_tensor, 8)
+    min_max_gd_example(weights_tensor, 8)
 
 